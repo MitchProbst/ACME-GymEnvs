8a88c0960dac16c5f0e8ec47e5d91a909412c52f
Mitchell Probst <mitchellprobst@gmail.com>
2018-03-26 22:11:30 -0600

    Remove unused environments

--- acme_gym/__init__.py
+++ acme_gym/__init__.py
@@ -18,20 +18,3 @@ register(
     reward_threshold=1.0,
     nondeterministic = True,
 )
-
-# Examples of other environments
-"""register(
-    id='SoccerEmptyGoal-v0',
-    entry_point='gym_soccer.envs:SoccerEmptyGoalEnv',
-    timestep_limit=1000,
-    reward_threshold=10.0,
-    nondeterministic = True,
-)
-
-register(
-    id='SoccerAgainstKeeper-v0',
-    entry_point='gym.envs:SoccerAgainstKeeperEnv',
-    timestep_limit=1000,
-    reward_threshold=8.0,
-    nondeterministic = True,
-)"""
